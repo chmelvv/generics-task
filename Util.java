e178e7a5f7c3f969aa06e872483bfca08e73ed02
Viktor Chmel <viktor.chmel@gmail.com>
2016-05-20 14:42:55 +0300

    add Util

--- Util.java
+++ Util.java
@@ -1,6 +1,3 @@
-/**
- * Created by Viktor_Chmel on 5/19/2016.
- */
 class Util {
     public static Product min(Product one, Product two) {
         return one.compareTo(two) < 0 ? one : one.compareTo(two) > 0 ? two : one;
